93811508dcd5843052fa6b9ab73b92fbe2c60ccf
Rick South <rick@natamus.com>
2023-12-12 10:34:54 +0100

    Custom Credits source code for 1.20.4.

---
+++ NeoForge/src/main/java/com/natamus/customcredits/neoforge/events/NeoForgeCommandEvents.java
@@ -0,0 +1,15 @@
+package com.natamus.customcredits.neoforge.events;
+
+import com.natamus.customcredits.cmds.CommandCredits;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber(value = Dist.CLIENT)
+public class NeoForgeCommandEvents {
+	@SubscribeEvent
+	public static void registerCommands(RegisterClientCommandsEvent e) {
+		CommandCredits.register(e.getDispatcher());
+	}
+}
