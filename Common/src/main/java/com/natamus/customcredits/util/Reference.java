4f82b309afcd7c00f0963f1a92a739f4e3376985
Rick South <rick@natamus.com>
2023-12-12 10:34:40 +0100

    Custom Credits source code for 1.20.1.

---
+++ Common/src/main/java/com/natamus/customcredits/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.customcredits.util;
+
+public class Reference {
+	public static final String MOD_ID = "customcredits";
+	public static final String NAME = "Custom Credits";
+	public static final String VERSION = "1.1";
+	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
+}
