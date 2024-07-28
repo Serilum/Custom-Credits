38eab47e1dab3e75034dc864f26b288ccd1b270b
Rick South <rick@natamus.com>
2024-07-28 07:37:57 +0200

    (v1.6) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/customcredits/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/customcredits/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.customcredits;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.customcredits.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.customcredits.neoforge.events.NeoForgeCommandEvents;
 import com.natamus.customcredits.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
