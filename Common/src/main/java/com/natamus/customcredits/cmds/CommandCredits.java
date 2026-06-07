e3f882e622ba0196a61057335691b56a5c0e4e7e
Rick South <38502394+ricksouth@users.noreply.github.com>
2026-06-07 19:36:39 +0200

    (v1.8) Updated the mod's text to use Collective's new translation system, which adds many languages alongside English! [+1]

--- Common/src/main/java/com/natamus/customcredits/cmds/CommandCredits.java
+++ Common/src/main/java/com/natamus/customcredits/cmds/CommandCredits.java
@@ -25,7 +25,7 @@ public class CommandCredits {
 			return 0;
 		}
 
-		player.displayClientMessage(Component.literal("Showing credits"), true);
+		player.displayClientMessage(Component.translatable("collective.customcredits.message.showingcredits"), true);
 
 		Constants.mc.schedule(() -> Constants.mc.setScreen(new WinScreen(false, () -> {
 			Constants.mc.setScreen(null);
